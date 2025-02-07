905f2d07886be19285d15740a4281d0cfba65f81
Salvatore Di Salvo <salvatore.disalvo@cardanofoundation.org>
2025-02-07 15:01:05 +0000

    fix: add tracker

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,21 @@ const App = () => {
       g.src = "http://localhost/js/container_3kFdwBcC.js";
       s.parentNode.insertBefore(g, s);
     })();
+    var _paq = (window._paq = window._paq || []);
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(["trackPageView"]);
+    _paq.push(["enableLinkTracking"]);
+    (function () {
+      var u = "//localhost/";
+      _paq.push(["setTrackerUrl", u + "matomo.php"]);
+      _paq.push(["setSiteId", "2"]);
+      var d = document,
+        g = d.createElement("script"),
+        s = d.getElementsByTagName("script")[0];
+      g.async = true;
+      g.src = u + "matomo.js";
+      s.parentNode.insertBefore(g, s);
+    })();
   }, []);
 
   return (
