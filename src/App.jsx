d6c44629d9984ae5b440319994bf9cd13ef96e59
Salvatore Di Salvo <salvatore.disalvo@cardanofoundation.org>
2025-02-07 16:44:46 +0000

    fix: testing trackers

--- src/App.jsx
+++ src/App.jsx
@@ -26,15 +26,12 @@ const App = () => {
   useEffect(() => {
     var _paq = (window._paq = window._paq || []);
     /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-    _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
-    _paq.push(["setCookieDomain", "*.sdisalvoreactapp.netlify.app"]);
-    _paq.push(["setDoNotTrack", true]);
     _paq.push(["trackPageView"]);
     _paq.push(["enableLinkTracking"]);
     (function () {
       var u = "//localhost/";
       _paq.push(["setTrackerUrl", u + "matomo.php"]);
-      _paq.push(["setSiteId", "3"]);
+      _paq.push(["setSiteId", "2"]);
       var d = document,
         g = d.createElement("script"),
         s = d.getElementsByTagName("script")[0];
