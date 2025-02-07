e81b3edefc8ec6c5ff099eff0b61be73b5609952
Salvatore Di Salvo <salvatore.disalvo@cardanofoundation.org>
2025-02-07 16:34:44 +0000

    fix: testing trackers

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,15 @@ const App = () => {
   const [landingPageData, setLandingPageData] = useState({});
   useEffect(() => {
     setLandingPageData(JsonData);
+
+    var _mtm = (window._mtm = window._mtm || []);
+    _mtm.push({ "mtm.startTime": new Date().getTime(), event: "mtm.Start" });
+    var d = document,
+      g = d.createElement("script"),
+      s = d.getElementsByTagName("script")[0];
+    g.async = true;
+    g.src = "http://localhost/js/container_SG4TbjDr.js";
+    s.parentNode.insertBefore(g, s);
   }, []);
 
   return (
